e60bc1d2c710ee167e040f701783deda56d304f7
Marcus Francisquini <francisquinimarcus@gmail.com>
2025-02-02 16:22:45 -0300

    navbar finalizada e otimizações feitas

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,19 +1,23 @@
 <template>
-  <q-header elevated class="header">
-    <q-toolbar class="toolbar">
-      <div class="navbar">
+  <q-header class="header bg-secondary row" elevated>
+    <q-toolbar class="toolbar justify-around">
+      <div class="row">
         <q-img class="logo-navbar" src="../assets/navbar-logo.png"></q-img>
-        <div class="labels">
-          <h6>Label</h6>
-          <h6>Label</h6>
-          <h6>Label</h6>
-          <h6>Label</h6>
+        <div class="labels row items-center q-pl-lg q-pt-sm gt-xs">
+          <h6>Label <q-icon name="arrow_drop_down" /></h6>
+          <h6>Label <q-icon name="arrow_drop_down" /></h6>
+          <h6>Label <q-icon name="arrow_drop_down" /></h6>
+          <h6>Label <q-icon name="arrow_drop_down" /></h6>
         </div>
       </div>
-      <q-btn class="btn-doacao" no-caps unelevated rounded color="primary">
-        <q-icon class="heart-icon" name="favorite" color="white" size="20px" />
+      <q-btn class="gt-xs" no-caps unelevated rounded color="primary">
+        <q-icon class="q-mr-sm" name="favorite" color="white" size="20px" />
         <div>Fazer doação</div>
       </q-btn>
+      <!-- menu hamburguer -->
+      <div class="xs">
+        <q-btn flat dense round icon="menu" aria-label="Menu" />
+      </div>
     </q-toolbar>
   </q-header>
 </template>
