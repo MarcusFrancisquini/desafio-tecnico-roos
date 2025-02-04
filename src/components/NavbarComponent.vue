2b07280546e622dc3e5ca154337525b5945c3c53
Marcus Francisquini <francisquinimarcus@gmail.com>
2025-02-04 01:18:21 -0300

    otimização finalizada e pronta para uso

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,24 +1,26 @@
 <template>
-  <q-header class="header bg-secondary row">
-    <q-toolbar class="toolbar justify-around">
-      <div class="row">
-        <q-img class="logo-navbar" src="../assets/navbar-logo.png"></q-img>
-        <div class="labels row items-center q-pl-lg q-pt-sm gt-xs">
-          <h6>Label <q-icon name="arrow_drop_down" /></h6>
-          <h6>Label <q-icon name="arrow_drop_down" /></h6>
-          <h6>Label <q-icon name="arrow_drop_down" /></h6>
-          <h6>Label <q-icon name="arrow_drop_down" /></h6>
+  <q-header class="header bg-secondary row justify-center">
+    <div class="row custom-spacing-border">
+      <q-toolbar class="toolbar row items-center justify-between no-padding">
+        <div class="row">
+          <q-img class="logo-navbar" src="../assets/navbar-logo.png"></q-img>
+          <div class="labels row items-center q-pl-lg q-pt-sm mobile-out">
+            <h6>Label <q-icon name="arrow_drop_down" /></h6>
+            <h6>Label <q-icon name="arrow_drop_down" /></h6>
+            <h6>Label <q-icon name="arrow_drop_down" /></h6>
+            <h6>Label <q-icon name="arrow_drop_down" /></h6>
+          </div>
         </div>
-      </div>
-      <q-btn class="gt-xs" no-caps unelevated rounded color="primary">
-        <q-icon class="q-mr-sm" name="favorite" color="white" size="20px" />
-        <div>Fazer doação</div>
-      </q-btn>
-      <!-- menu hamburguer -->
-      <div class="xs">
-        <q-btn flat dense round icon="menu" aria-label="Menu" />
-      </div>
-    </q-toolbar>
+        <q-btn class="mobile-out" no-caps unelevated rounded color="primary">
+          <q-icon class="q-mr-sm" name="favorite" color="white" size="20px" />
+          <div>Fazer doação</div>
+        </q-btn>
+        <!-- menu hamburguer -->
+        <div class="menu-mobile">
+          <q-btn flat dense round icon="menu" aria-label="Menu" />
+        </div>
+      </q-toolbar>
+    </div>
   </q-header>
 </template>
 
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,53 +1,57 @@
 <template>
   <!-- Hero -->
-  <section class="hero row justify-center items-center custom-spacing-border">
-    <div class="container-hero flex items-end">
-      <div class="box-shadow-hero"></div>
-      <div class="hero-content">
-        <h5 class="text-white text-weight-bold">
-          Solidariedade em Ação: ONG Apoia Comunidade Haitiana na Luta por Direitos e Dignidade!
-        </h5>
-        <p class="text-white text-weight-light q-pt-sm">
-          Lorem ipsum dolor sit amet. In molestiae quaerat ut dolores quia qui repellendus impedit
-          et quibusdam excepturi. Ut mollitia accusamus in sint provident quo voluptatem distinctio
-          aut fugiat quas non itaque debitis rem quia harum. In corpo
-        </p>
-        <q-btn no-caps unelevated rounded color="primary">
-          <q-icon class="q-mr-sm" name="favorite" color="white" size="20px" />
-          <div>Fazer doação</div>
-        </q-btn>
+  <section class="hero row justify-center">
+    <div class="custom-spacing-border">
+      <div class="container-hero flex items-end">
+        <div class="box-shadow-hero"></div>
+        <div class="hero-content">
+          <h5 class="text-white text-weight-bold">
+            Solidariedade em Ação: ONG Apoia Comunidade Haitiana na Luta por Direitos e Dignidade!
+          </h5>
+          <p class="text-white text-weight-light q-pt-sm">
+            Lorem ipsum dolor sit amet. In molestiae quaerat ut dolores quia qui repellendus impedit
+            et quibusdam excepturi. Ut mollitia accusamus in sint provident quo voluptatem
+            distinctio aut fugiat quas non itaque debitis rem quia harum. In corpo
+          </p>
+          <q-btn no-caps unelevated rounded color="primary">
+            <q-icon class="q-mr-sm" name="favorite" color="white" size="20px" />
+            <div>Fazer doação</div>
+          </q-btn>
+        </div>
       </div>
     </div>
   </section>
   <!-- Section Transformar -->
   <section class="section-transformar bg-black row justify-center">
-    <div class="cards-projetos row items-center justify-between">
-      <h6 class="text-white">Ajudamos a transformar...</h6>
-      <div class="card row bg-white q-px-md q-py-md items-center">
-        <q-avatar size="40px" color="green-1" text-color="green-2" font-size="18px">
-          <q-icon name="fa-solid fa-arrow-up" />
-        </q-avatar>
-        <div class="card-text q-pl-sm">
-          <p class="card-text-num text-primary text-weight-medium no-margin">+35</p>
-          <p class="card-text-info text-grey no-margin">Projetos realizados</p>
+    <div class="custom-spacing-border row">
+      <div class="cards-projetos row items-center justify-between">
+        <h6 class="text-white">Ajudamos a transformar...</h6>
+        <div class="card row bg-white q-px-md q-py-md items-center">
+          <q-avatar size="40px" color="green-1" text-color="green-2" font-size="18px">
+            <q-icon name="fa-solid fa-arrow-up" />
+          </q-avatar>
+          <div class="card-text q-pl-sm">
+            <p class="card-text-num text-primary text-weight-medium no-margin">+35</p>
+            <p class="card-text-info text-grey no-margin">Projetos realizados</p>
+          </div>
         </div>
-      </div>
-      <div class="card row bg-white q-px-md q-py-md items-center">
-        <q-avatar size="40px" color="green-1" text-color="green-2" font-size="18px">
-          <q-icon name="fa-solid fa-arrow-up" />
-        </q-avatar>
-        <div class="card-text q-pl-sm">
-          <p class="card-text-num text-primary text-weight-medium no-margin">+35</p>
-          <p class="card-text-info text-grey no-margin">Projetos realizados</p>
+        <div class="card row bg-white q-px-md q-py-md items-center">
+          <q-avatar size="40px" color="green-1" text-color="green-2" font-size="18px">
+            <q-icon name="fa-solid fa-arrow-up" />
+          </q-avatar>
+          <div class="card-text q-pl-sm">
+            <p class="card-text-num text-primary text-weight-medium no-margin">+35</p>
+            <p class="card-text-info text-grey no-margin">Projetos realizados</p>
+          </div>
         </div>
-      </div>
-      <div class="card row bg-white q-px-md q-py-md items-center">
-        <q-avatar size="40px" color="green-1" text-color="primary" font-size="18px">
-          <q-icon name="fa-solid fa-arrow-up" />
-        </q-avatar>
-        <div class="card-text q-pl-sm">
-          <p class="card-text-num text-primary text-weight-medium no-margin">+135.958</p>
-          <p class="card-text-info text-grey no-margin">Pessoas impactadas</p>
+        <div class="card row bg-white q-px-md q-py-md items-center">
+          <q-avatar size="40px" color="green-1" text-color="primary" font-size="18px">
+            <q-icon name="fa-solid fa-arrow-up" />
+          </q-avatar>
+          <div class="card-text q-pl-sm">
+            <p class="card-text-num text-primary text-weight-medium no-margin">+135.958</p>
+            <p class="card-text-info text-grey no-margin">Pessoas impactadas</p>
+          </div>
         </div>
       </div>
     </div>
